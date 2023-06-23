c7a86de1bd1f83c428354f35d3b1cb3aff4b48f1
Bruno Hirata <bruno.n.hirata@outlook.com>
2023-06-22 21:21:52 -0300

    Bruno - Post

---
+++ src/routes/routes.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const postController = require("../controllers/controllers")
+
+const router = express.Router()
+router.get("/exibir", postController.exibirTodos)
+router.get("/id/:id", postController.exibirPorId)
+router.post("/criar", postController.cadastrarAluno)
+
+module.exports = router
